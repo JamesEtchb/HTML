8d8de8f0418ed144841a130f6f98cfb11764423e
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-08-01 14:40:51 -0400

    added more stuff

--- students.js
+++ students.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
 const app = express()
 const port = 3030
@@ -6,10 +6,19 @@ const port = 3030
 app.use(cors())
 app.use(express.json())
 
+let students = ['bob', 'samantha', 'lily', 'grace', 'jim']
+
 app.get('/', (req, res) => {
-    res.send('Hello World ... again!!!')
+  let output = '<html><body><ul><h3>'
+  //   output = output + 'Hello there html'
+  for (let i = 0; i < students.length; i++) {
+    output = output + '<li><input type="checkbox">' + students[i] + '</li>'
+  }
+  output = output + '</h3></ul></body></html>'
+
+  res.send(output)
 })
 
 app.listen(port, () => {
-    console.log(`I'm needed to run on port:${port}`)
+  console.log(`Running on port:${port}`)
 })
